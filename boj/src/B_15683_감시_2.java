6901d45e67065ad9934623cc58804e3e76d545fb
euni <ktt7566@naver.com>
2020-02-04 00:16:40 +0900

    20200204

--- boj/src/B_15683_감시_2.java
+++ boj/src/B_15683_감시_2.java
@@ -1,11 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class B_15683_감시_2_X {
-	static int N, M;
+public class B_15683_감시_2 {
+	static int N, M, deadzone, min;
+	static int[] order;
 	static int[][] office;
 	static ArrayList<Point> cctv;
 	static int[][] dir = { {}, { 1, 2, 4, 8 }, { 5, 10 }, { 3, 6, 12, 9 }, { 7, 14, 13, 11 }, { 15 } };
@@ -18,23 +18,24 @@ public class B_15683_감시_2_X {
 		M = Integer.parseInt(st.nextToken());
 		office = new int[N][M];
 		cctv = new ArrayList<>();
+		min = N * M;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				office[i][j] = Integer.parseInt(st.nextToken());
-				if (office[i][j] <= 5 && office[i][j] >= 1)
-					cctv.add(new Point(i, j, office[i][j]));
+				if (office[i][j] <= 5 && office[i][j] >= 1) cctv.add(new Point(i, j, office[i][j]));
+				else if(office[i][j] == 0) deadzone++;
 			}
 		}
 		order = new int[cctv.size()];
-		dfs(0);
+		perm(0);
+		System.out.println(min);
 	}
 
-	static int[] order;
 
-	private static void dfs(int idx) {
+	private static void perm(int idx) {
 		if(idx == cctv.size()) {
-			System.out.println(Arrays.toString(order));
+			go();
 			return;
 		}
 			int temp = cctv.get(idx).dir;
@@ -44,8 +45,36 @@ public class B_15683_감시_2_X {
 			
 			for(int i = 0; i < limit; i++) {
 				order[idx] = dir[temp][i];
-				dfs(idx + 1);
+				perm(idx + 1);
 			}
+	}
+	static int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1}; // 상, 우, 하, 좌
+	private static void go() {
+		boolean[][] visited = new boolean[N][M];
+		int dz_temp = deadzone;
+		
+		for(int i = 0; i < cctv.size(); i++) {
+			Point temp = cctv.get(i);
+			for(int j = 0; j < 4; j++) {
+				if(((1 << j) & order[i]) > 0) {
+					int tx = temp.x;
+					int ty = temp.y;
+
+					while(true) {
+						tx += dx[j];
+						ty += dy[j];
+						
+						if(tx < 0 || ty < 0 || tx >= N || ty >= M) break;
+						if(office[tx][ty] == 6) break;
+						if(office[tx][ty] == 0 && !visited[tx][ty]) {
+							dz_temp--;
+							visited[tx][ty] = true;
+						}
+					}
+				}
+			}
+		}
+		min = Math.min(min, dz_temp);
 	}
 
 	static class Point {
@@ -55,11 +84,6 @@ public class B_15683_감시_2_X {
 			this.x = x;
 			this.y = y;
 			this.dir = dir;
-		}
-
-		@Override
-		public String toString() {
-			return "Point [x=" + x + ", y=" + y + ", dir=" + dir + "]";
 		}
 
 	}
